2f9745f1f88dfde3ef003a126088d7257d4a7853
Brad McDermott <bradmcdermott@gmail.com>
2017-08-01 14:57:50 -0400

    Adds item model and db migrations

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 
+from lists.models import Item
 from lists.views import home_page
 
 
@@ -18,3 +19,23 @@ class SmokeTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEquals(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEquals(first_saved_item.text, 'The first (ever) list item')
+        self.assertEquals(second_saved_item.text, 'Item the second')
