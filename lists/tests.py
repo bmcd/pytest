fe4fc6aae26567e3d85840b1e4ed34c1c2f7c699
Brad McDermott <bradmcdermott@gmail.com>
2017-08-01 14:41:38 -0400

    Adds item to template on POST

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,8 @@
 from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
 
 from lists.views import home_page
+
 
 class SmokeTest(TestCase):
 
@@ -12,4 +12,9 @@ class SmokeTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
