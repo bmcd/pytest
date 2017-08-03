608c6fa482d2c7f2d299e0f26475b5e9cc45b7d1
bmcd87 <bradmcdermott@gmail.com>
2017-08-03 14:30:40 -0400

    Adds list model

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -16,8 +16,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/theonlylistintheworld/')
 
@@ -25,24 +26,33 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 2')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save() 
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
 
+        saved_list = List.objects.first()
+        self.assertEquals(saved_list, list_)
         saved_items = Item.objects.all()
         self.assertEquals(saved_items.count(), 2)
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEquals(first_saved_item.text, 'The first (ever) list item')
+        self.assertEquals(first_saved_item.list, list_)
         self.assertEquals(second_saved_item.text, 'Item the second')
+        self.assertEquals(second_saved_item.list, list_)
 
 
 class NewListTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from lists.models import Item
+from lists.models import Item, List
 
 
 def home_page(request):
@@ -12,5 +12,6 @@ def view_list(request):
     return render(request, 'list.html', {'items': items})
 
 def new_list(request):
-    Item.objects.create(text=request.POST.get('item_text', ''))
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST.get('item_text', ''), list=list_)
     return redirect('/lists/theonlylistintheworld/')
